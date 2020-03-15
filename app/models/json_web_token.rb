d050b18242daf19640d88319db5fdaae1ef60e94
alexbispo <alexbispo.artes@gmail.com>
2020-03-15 10:33:02 +0000

    Add more enviroment variables.

--- app/models/json_web_token.rb
+++ app/models/json_web_token.rb
@@ -1,10 +1,13 @@
 class JsonWebToken
+
+  JWT_KEY = ENV['JWT_KEY'] || Rails.application.secrets.secret_key_base
+
   def self.encode(payload)
     expiration = 30.minutes.from_now.to_i
-    JWT.encode payload.merge(exp: expiration), Rails.application.secrets.secret_key_base
+    JWT.encode(payload.merge(exp: expiration),  JWT_KEY)
   end
 
   def self.decode(token)
-    JWT.decode(token, Rails.application.secrets.secret_key_base).first
+    JWT.decode(token, JWT_KEY).first
   end
 end
