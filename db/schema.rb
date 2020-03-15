feb8502541dc2e0f12bd20225907b6e4648f5e65
alexbispo <alexbispo.artes@gmail.com>
2020-03-15 19:44:04 +0000

    Adds migrations to active storage database.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_14_094503) do
+ActiveRecord::Schema.define(version: 2020_03_15_193324) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +31,14 @@ ActiveRecord::Schema.define(version: 2020_03_14_094503) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_data", force: :cascade do |t|
+    t.string "key", null: false
+    t.binary "io", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["key"], name: "index_active_storage_data_on_key"
   end
 
   create_table "pictures", force: :cascade do |t|
