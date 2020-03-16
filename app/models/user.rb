e36c6afb44a7a3b74698c270a63113b9a7515366
alexbispo <alexbispo.artes@gmail.com>
2020-03-16 06:17:07 +0000

    More tests to user model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,13 @@ class User < ApplicationRecord
   has_secure_password
   has_many :pictures
 
+  validates :email, :username, presence: true, uniqueness: true
 
-  validates :email, :username, presence: true
   validates :email, format: { with: EMAIL_REGEXP, message: 'invalid email' }
+
+  validates :password, length: { in: 8..20 }
+
+  validates :username, length: { in: 3..20 }
 
   def valid_password?(password)
     self.authenticate(password)
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,11 +4,13 @@ class UserTest < ActiveSupport::TestCase
 
   test "should be possible create a user" do
     user = User.create(username: 'mozart', email: 'mozart@test.com', password: 'tempe!123')
+
     assert user.errors.empty?
   end
 
   test "email should be present." do
     user = User.create(email: '')
+
     assert user.errors[:email].any?
   end
 
@@ -19,21 +21,58 @@ class UserTest < ActiveSupport::TestCase
   ].each do |email|
     test "email (#{email}) should be invalid." do
       user = User.create(email: email)
+
       assert user.errors[:email].any?
     end
   end
 
   test "username should be present." do
     user = User.create(username: '')
+
     assert user.errors[:username].any?
   end
 
   test "password should be present." do
     user = User.create(password: '')
+
     assert user.errors[:password].any?
   end
 
-  # test "username should be unique" do
+  test "username should be unique" do
+    user1 = User.create(username: 'mozart', email: 'mozart@test.com', password: 'tempe!123')
+    user2 = User.create(username: 'mozart', email: 'mozart@test.com', password: 'tempe!123')
 
-  # end
+    assert user2.errors[:username].any?
+  end
+
+  test "email should be unique" do
+    user1 = User.create(username: 'mozart', email: 'mozart@test.com', password: 'tempe!123')
+    user2 = User.create(username: 'mozart1988', email: 'mozart@test.com', password: 'tempe!123')
+
+    assert user2.errors[:email].any?
+  end
+
+  test "password has at last 8 characters" do
+    user = User.create(password: 'tempe')
+
+    assert user.errors[:password].any?
+  end
+
+  test "password has at maximum 20 characters" do
+    user = User.create(password: 't' * 21)
+
+    assert user.errors[:password].any?
+  end
+
+  test "username has at least 3 characters" do
+    user = User.create(username: 'mo')
+
+    assert user.errors[:username].any?
+  end
+
+  test "username has at maximum 20 characters" do
+    user = User.create(username: 'm' * 21 )
+
+    assert user.errors[:username].any?
+  end
 end
